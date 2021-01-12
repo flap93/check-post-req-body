b57a594a5e3ee0a81a334a181455f76e4fc3ef7b
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2021-01-12 17:07:43 -0500

    completed

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
 const express = require('express');
-
 const hbs = require('hbs');
-
 require('dotenv').config();
 
 const app = express();
-const bodyParser = require('body-parser')
-// ****************** require the necessary npm package here: ******************
 
-app.use(express.urlencoded({ extended: true}));
+// ****************** require body-parser here: ******************
+// ****************** require the necessary npm package here: ******************
+// ... your code here
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+// *********************************************************
 
 // *****************************************************************************
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
-
 app.use(express.static('public'));
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 // 4.
+// ... your code here
 app.post('/create', (req, res) => {
-  const { theAge} = req.body;
-console.log(`User's age is ${theAge}`);
-
+  const { userAge } = req.body;
+  console.log(`User's input: ${userAge}`);
 });
 
 // 5:
 // ... your code here
+app.post('/new/user', (req, res) => {
+  const { firstName, lastName, street, number, city, zip, country } = req.body;
+  console.log(
+    `User's input:`,
+    req.body,
+    firstName,
+    lastName,
+    street,
+    number,
+    zip,
+    country
+  );
+});
 
 app.listen(process.env.PORT, () =>
-  console.log(`Running on port: ${process.env.PORT}`)
-);
+  console.log(`Running on port: ${process.env.PORT}`));
